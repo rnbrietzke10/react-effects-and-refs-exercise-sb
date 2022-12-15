f0065e9066e2d3c93de94d264900f240760f7611
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-12-15 11:15:18 -0600

    set up get deck useEffect

--- src/Deck.js
+++ src/Deck.js
@@ -1,17 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Card from './Card';
 import './Deck.css';
 
 const Deck = () => {
-  const [deck, setDeck] = useState([]);
+  const [deck, setDeck] = useState(null);
+  const [draw, setDraw] = useState([]);
+
+  useEffect(() => {
+    const getDeck = async () => {
+      let newDeck = await axios.get(
+        `http://deckofcardsapi.com/api/deck/new/shuffle/`
+      );
+      setDeck(newDeck.data);
+    };
+    getDeck();
+  });
+
   return (
-    <div className="deck-container">
+    <div className='deck-container'>
       <header>
-        <button className="btn" id="get-card">
+        <button className='btn' id='get-card'>
           Get a Card
         </button>
       </header>
-      {deck.map((card) => (
+      {deck.map(card => (
         <Card />
       ))}
     </div>
